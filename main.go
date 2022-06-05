d209d612fd84310934281a032b50e5da84975fd2
DeltaLaboratory <delta@deltalab.dev>
2022-06-05 18:44:23 +0900

    Something

--- main.go
+++ main.go
@@ -28,21 +28,31 @@ const LauncherVersion = "0.0.6-dev"
 
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-	log.Info().Msgf("Hentai@Home Container Version %s\n", LauncherVersion)
-	log.Info().Msgf("Hentai@Home Client Version %s\n", HathVersion)
+	log.Info().Str("Task", "Main").Msgf("Hentai@Home Container Version %s", LauncherVersion)
+	log.Info().Str("Task", "Main").Msgf("Hentai@Home Client Version %s", HathVersion)
 	if _, err := os.Stat("/hath/hath.jar"); err != nil {
-		fmt.Println("Hentai@Home is Missing, Download New One...")
+		log.Warn().Str("Task", "Main").Msg("Hentai@Home is Missing, Download New One...")
 		download()
 	}
 	if os.Getenv("CLIENT_ID") == "" || os.Getenv("CLIENT_KEY") == "" {
-		fmt.Println("Client ID or Client Key is not set Properly.")
+		log.Fatal().Msg("Client ID or Client Key is not set Properly.")
 		os.Exit(30)
 	}
 	createCredential(os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_KEY"))
-	log.Info().Msgf("Starting Hentai@Home Process...")
+	log.Info().Str("Task", "Main").Msgf("Starting Hentai@Home Process...")
 	process := exec.Command("java", "-jar", "/hath/hath.jar")
+	if os.Getenv("WAIT_BEFORE_START") != "" {
+		wait, err := strconv.Atoi(os.Getenv("WAIT_BEFORE_START"))
+		if err != nil {
+			log.Fatal().Str("Task", "Main").Msgf("Cannot Parse WAIT_BEFORE_START : %s", wait)
+			os.Exit(50)
+		}
+		log.Info().Str("Task", "Main").Msgf("Wait %ss Before Starting...", os.Getenv("WAIT_BEFORE_START"))
+		time.Sleep(time.Second * time.Duration(wait))
+		log.Info().Str("Task", "Main").Msgf("Done.", os.Getenv("WAIT_BEFORE_START"))
+	}
 	if err := process.Start(); err != nil {
-		fmt.Printf("An Error was Occured while Staring Hentai@Home Process : %s\n", err)
+		log.Fatal().Str("Task", "Main").Msgf("An Error was Occurred while Staring Hentai@Home Process : %s", err)
 		os.Exit(35)
 	}
 	done := make(chan any)
@@ -57,31 +67,31 @@ func main() {
 			return
 		}
 		var retry int
-		fmt.Println("Creating PEM-Encoded Certificate...")
+		log.Info().Str("Task", "Cert Generating").Msg("Creating PEM-Encoded Certificate...")
 		for {
 			trustStore, err := ioutil.ReadFile("/hath/data/hathcert.p12")
 			if err != nil {
 				retry += 15
-				fmt.Printf("Could Not Read Hath Network Cert, Try After %s", strconv.Itoa(retry))
+				log.Warn().Str("Task", "Cert Generating").Msgf("Could Not Read Hath Network Cert, Try After %s", strconv.Itoa(retry))
 				time.Sleep(time.Second * time.Duration(retry))
 				continue
 			}
 			privateKey, cert, ca, err := pkcs12.DecodeChain(trustStore, os.Getenv("CLIENT_KEY"))
 			if err != nil {
-				fmt.Printf("Could Not Decode Trust Store : %s\n", err)
+				log.Error().Str("Task", "Cert Generating").Msgf("Could Not Decode Trust Store : %s", err)
 				return
 			}
-			fmt.Printf("Certificate DN=%s\n", cert.DNSNames[0])
-			fmt.Printf("Certificate SN=%s\n", cert.SerialNumber)
-			fmt.Printf("Certificate Algo=%s\n", cert.PublicKeyAlgorithm)
-			fmt.Printf("Certificate Expire=%s\n", cert.NotAfter.String())
+			log.Info().Str("Task", "Cert Generating").Msgf("Certificate DN=%s", cert.DNSNames[0])
+			log.Info().Str("Task", "Cert Generating").Msgf("Certificate SN=%s", cert.SerialNumber)
+			log.Info().Str("Task", "Cert Generating").Msgf("Certificate Algo=%s", cert.PublicKeyAlgorithm)
+			log.Info().Str("Task", "Cert Generating").Msgf("Certificate Expire=%s", cert.NotAfter.String())
 			if err := os.MkdirAll("/cert", 0644); err != nil {
-				fmt.Printf("Failed to Create Cert File Directory : %s\n", err)
+				log.Error().Str("Task", "Cert Generating").Msgf("Failed to Create Cert File Directory : %s", err)
 				return
 			}
 			privateKeyDER, err := x509.MarshalPKCS8PrivateKey(privateKey)
 			if err != nil {
-				fmt.Printf("Could Not Encode PrivateKey : %s\n", err)
+				log.Error().Str("Task", "Cert Generating").Msgf("Could Not Encode PrivateKey : %s", err)
 				return
 			}
 			var caBuffer bytes.Buffer
@@ -96,42 +106,42 @@ func main() {
 				Type:  "CERTIFICATE",
 				Bytes: cert.Raw,
 			}), 0644); err != nil {
-				fmt.Printf("Could Not Write Cert PEM : %s\n", err)
+				log.Error().Str("Task", "Cert Generating").Msgf("Could Not Write Cert PEM : %s", err)
 				return
 			}
 			if err = ioutil.WriteFile("/cert/privkey.pem", pem.EncodeToMemory(&pem.Block{
 				Type:  "PRIVATE KEY",
 				Bytes: privateKeyDER,
 			}), 0644); err != nil {
-				fmt.Printf("Could Not Write Private Key : %s\n", err)
+				log.Error().Str("Task", "Cert Generating").Msgf("Could Not Write Private Key : %s", err)
 				return
 			}
 			if err = ioutil.WriteFile("/cert/chain.pem", caPEM, 0644); err != nil {
-				fmt.Printf("Could Not Write CA Chain PEM : %s\n", err)
+				log.Error().Str("Task", "Cert Generating").Msgf("Could Not Write CA Chain PEM : %s", err)
 				return
 			}
 			if err = ioutil.WriteFile("/cert/fullchain.pem", append(pem.EncodeToMemory(&pem.Block{
 				Type:  "CERTIFICATE",
 				Bytes: cert.Raw,
 			}), caPEM...), 644); err != nil {
-				fmt.Printf("Could Not Write Fullchain PEM : %s\n", err)
+				log.Error().Str("Task", "Cert Generating").Msgf("Could Not Write Fullchain PEM : %s", err)
 				return
 			}
 			err = ioutil.WriteFile("/cert/host", []byte(cert.DNSNames[0]), 0644)
 			if err != nil {
-				fmt.Printf("Could Not Write Hentai@Home Network Host File : %s\n", err)
+				log.Error().Str("Task", "Cert Generating").Msgf("Could Not Write Hentai@Home Network Host File : %s", err)
 				return
 			}
-			fmt.Println("Certificate Generated!")
+			log.Info().Str("Task", "Cert Generating").Msg("Certificate Generated!")
 			return
 		}
 	}()
 	// Shutdown Handler
 	go func() {
 		<-sigChannel
-		fmt.Println("Shutting Down Hentai@Home...")
+		log.Info().Str("Task", "Graceful Shutdown").Msg("Shutting Down Hentai@Home...")
 		if err := process.Process.Signal(syscall.SIGINT); err != nil {
-			fmt.Printf("An Error was Occured while Shutting Down Hentai@Home Process : %s\n", err)
+			log.Fatal().Str("Task", "Graceful Shutdown").Msgf("An Error was Occurred while Shutting Down Hentai@Home Process : %s", err)
 			done <- nil
 			return
 		}
@@ -141,7 +151,7 @@ func main() {
 	}()
 	go func() {
 		err := process.Wait()
-		fmt.Printf("Hentai@Home Process Exited : %s", err.Error())
+		log.Info().Str("Task", "Graceful Shutdown").Msgf("Hentai@Home Process Exited : %s", err.Error())
 		done <- nil
 	}()
 	<-done
@@ -161,29 +171,29 @@ func download() {
 		}
 	}()
 	if err != nil {
-		fmt.Printf("Failed to Download Hentai@Home : %s\n", err)
+		log.Fatal().Str("Task", "Download Hentai@Home").Msgf("Failed to Download Hentai@Home : %s\n", err)
 		os.Exit(10)
 	}
 	bar := progressbar.DefaultBytes(res.ContentLength, "Downloading Hentai@Home")
 	if _, err = io.Copy(io.MultiWriter(bar, &fileWriter), res.Body); err != nil {
-		fmt.Printf("Failed to Download Hentai@Home : %s\n", err)
+		log.Fatal().Str("Task", "Download Hentai@Home").Msgf("Failed to Download Hentai@Home : %s\n", err)
 		os.Exit(10)
 	}
 	file := fileWriter.Bytes()
 	zipReader, _ := zip.NewReader(bytes.NewReader(file), int64(len(file)))
 	f, err := zipReader.Open("HentaiAtHome.jar")
 	if err != nil {
-		fmt.Printf("Failed to Extract Hentai@Home : %s\n", err)
+		log.Fatal().Str("Task", "Download Hentai@Home").Msgf("Failed to Extract Hentai@Home : %s\n", err)
 		os.Exit(15)
 	}
 	hathFile, err := io.ReadAll(f)
 	if err != nil {
-		fmt.Printf("Failed to Extract Hentai@Home : %s\n", err)
+		log.Fatal().Str("Task", "Download Hentai@Home").Msgf("Failed to Extract Hentai@Home : %s\n", err)
 		os.Exit(15)
 	}
 	err = ioutil.WriteFile("/hath/hath.jar", hathFile, 0655)
 	if err != nil {
-		fmt.Printf("Could Not Write Hentai@Home File : %s\n", err)
+		log.Fatal().Str("Task", "Download Hentai@Home").Msgf("Could Not Write Hentai@Home File : %s\n", err)
 		os.Exit(20)
 	}
 	fmt.Println("Download Completed")
@@ -192,17 +202,17 @@ func download() {
 
 func createCredential(clientId, clientKey string) {
 	if err := os.MkdirAll("/hath/data", 0644); err != nil {
-		fmt.Printf("Failed to Create Credential File Directory : %s\n", err)
+		log.Fatal().Str("Task", "Credential Creation").Msgf("Failed to Create Credential File Directory : %s", err)
 		os.Exit(40)
 	}
 	fs, err := os.OpenFile("/hath/data/client_login", os.O_CREATE|os.O_WRONLY, 644)
 	if err != nil {
-		fmt.Printf("Failed to Open Credential File : %s\n", err)
+		log.Fatal().Str("Task", "Credential Creation").Msgf("Failed to Open Credential File : %s", err)
 		os.Exit(20)
 	}
 	_, err = fs.WriteString(fmt.Sprintf("%s-%s", clientId, clientKey))
 	if err != nil {
-		fmt.Printf("Failed to Write Credential File : %s\n", err)
+		log.Fatal().Str("Task", "Credential Creation").Msgf("Failed to Write Credential File : %s", err)
 		os.Exit(25)
 	}
 }
